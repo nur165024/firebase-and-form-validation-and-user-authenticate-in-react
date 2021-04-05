1781b93523d5f087d6060db0054b6247eef63969
nur165024 <nur42658@gmail.com>
2021-04-05 16:44:52 +0600

    firebase auth done

--- src/App.js
+++ src/App.js
@@ -7,15 +7,19 @@ import { useState } from 'react';
 firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [newUser, setNewUser] = useState(false);
   const [user,setUser] = useState({
     isSignedIn : false,
     name : '',
     email : '',
-    photo : ''
+    password : '',
+    photo : '',
+    error : '',
+    success : false,
   })
 
   const provider = new firebase.auth.GoogleAuthProvider();
-
+  // firebase signin
   const signInClick = () => {
     firebase.auth().signInWithPopup(provider)
     .then(res => {
@@ -30,10 +34,11 @@ function App() {
       }
       setUser(userSignIn);
     }).catch(err => {
-      console.log(err.message);
+      // console.log(err.message);
     })
   }
-
+  
+  // firebase signout
   const signOutClick = () => {
     firebase.auth().signOut()
     .then(res => {
@@ -46,8 +51,65 @@ function App() {
       }
       setUser(userSignOut);
     }).catch(err => {
-      console.log(err.message);
+      // console.log(err.message);
     })
+  }
+
+  // input field validation
+  const inputFieldBlur = (e) => {
+    let fieldValidation = true;
+    if (e.target.name === 'email') {
+      const pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+      fieldValidation = pattern.test(e.target.value);
+    }
+
+    if (e.target.name === 'password') {
+      const userPasswordLength = e.target.value.length > 6;
+      const userPasswordRx = /^[A-Za-z]\w{7,}$/.test(e.target.value);
+      fieldValidation = userPasswordLength && userPasswordRx;
+    }
+
+    let userInfo = {...user};
+    if (fieldValidation) { 
+      userInfo[e.target.name] = e.target.value;
+      setUser(userInfo);
+    }
+  }
+
+  // firebase form submit
+  const formSubmitUser = (e) => {
+    if (newUser && user.email && user.password) {
+      firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+      .then(res => {
+        const newUserInfo = {...user}
+        newUserInfo.error = ''     
+        newUserInfo.success = true        
+        setUser(newUserInfo)
+      })
+      .catch(err => {
+        const newUserInfo = {...user}
+        newUserInfo.error = err.message;        
+        newUserInfo.success = '';        
+        setUser(newUserInfo);
+      });
+    }
+
+    if (!newUser && user.email && user.password) {
+      firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+      .then(res => {
+        const logInUser = {...user}
+        logInUser.error = ''
+        logInUser.success = true     
+        setUser(logInUser)
+      })
+      .catch(err => {
+        const logInUser = {...user}
+        logInUser.error = err.message;
+        logInUser.success = ''; 
+        setUser(logInUser);
+      });
+    }
+    e.preventDefault();
   }
 
   return (
@@ -64,6 +126,30 @@ function App() {
           <img src={user.photo} alt=""/>
         </div>
       }
+      <br/>
+      <h3>Create new User</h3>
+      <span style={{color:'red'}}>{user.error}</span>
+      {
+        user.success && <span style={{color:'green'}}>User {newUser ? 'Created' : 'Logged In'} Successfully!</span>
+      }
+      <br/>
+      <form onSubmit={formSubmitUser}>
+        <input type="checkbox" onChange={() => setNewUser(!newUser)} /> Create new User
+        <br/>
+        {newUser && <input required onBlur={inputFieldBlur} type="text" name="name" placeholder="Enter your Name" />}
+        <br/>
+        <input required onBlur={inputFieldBlur} type="text" name="email" placeholder="Enter your Email address" />
+        <br/>
+        <input required onBlur={inputFieldBlur} type="password" name="password" placeholder="Enter your password" />
+        <br/>
+        <br/>
+        {
+          !newUser 
+          ? <input type="submit" value="Logged In"/>
+          : <input type="submit" value="Signup"/>
+        }
+        
+      </form>
     </div>
   );
 }
